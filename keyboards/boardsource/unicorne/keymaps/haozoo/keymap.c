d6746cc9e190d67b5c5d0de7b84d4b27c9ad1b29
Hao Xu <hxu@atlassian.com>
2023-12-15 17:41:52 +1100

    ported more complex behaviour over

--- keyboards/boardsource/unicorne/keymaps/haozoo/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/haozoo/keymap.c
@@ -6,30 +6,150 @@
 #define _SYMS 2
 #define _MOVE 3
 
+#define SS_HYPER(KC) SS_LCMD(SS_LCTL(SS_LOPT(SS_LSFT(KC))))
+
+#define L1_THUMB LT(_NUMS, KC_SPC)
+#define R1_THUMB LT(_MOVE, KC_BSPC)
+#define R2_THUMB LT(_SYMS, KC_ENT)
+
+#define LHS_WORD LOPT(KC_LEFT)
+#define RHS_WORD ROPT(KC_RIGHT)
+
+enum custom_keycodes {
+    _____ = SAFE_RANGE,
+	MACST,  // MacOS screenshot to clipboard snipping tool 
+	ARCST,  // Arc browser UI component snipping tool
+	ARC1,	// Arc browser fast pinned tab navigation...
+	ARC2, 
+	ARC3,
+	ARC4,
+	ARC5,
+	ARC6,
+	APP1,	// MacOS fast application navigation...
+	APP2,
+	APP3,
+	AMSL,   // Amethyst switch layouts 
+	AMSM,	// Amethyst switch main window 
+	AMIN, 	// Amethyst increase main window's horizontal width 
+	AMDE,   // Amethyst decrease main window's horizontal width
+};
+
+enum tap_dances {
+    TD_V,
+	TD_CBR,
+	TD_BRC,
+	TD_PRN,
+};
+
+// Combos 
+const uint16_t PROGMEM alfred_search_combo[] = {KC_SPC, KC_BSPC, COMBO_END};
+const uint16_t PROGMEM wooshy_search_combo[] = {KC_B, KC_N, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(alfred_search_combo, LCMD(KC_SPC)),
+    COMBO(wooshy_search_combo, HYPR(KC_W)),
+};
+
+// Key Overrides AKA mod-morphs: 
+const key_override_t amth_cycl_override = ko_make_basic(MOD_MASK_SHIFT, AMSM, AMSL);
+const key_override_t backspace_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, LOPT(KC_BSPC));
+const key_override_t copypaste_override = ko_make_basic(MOD_MASK_GUI, KC_V, TD_V);
+const key_override_t grave_esc_override = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, KC_GRV);
+const key_override_t snip_tool_override = ko_make_basic(MOD_MASK_SHIFT, MACST, ARCST);
+const key_override_t arc_app_1_override = ko_make_basic(MOD_MASK_SHIFT, ARC1, APP1);
+const key_override_t arc_app_2_override = ko_make_basic(MOD_MASK_SHIFT, ARC2, APP2);
+const key_override_t arc_app_3_override = ko_make_basic(MOD_MASK_SHIFT, ARC3, APP3);
+const key_override_t **key_overrides = (const key_override_t *[]){
+	&amth_cycl_override,
+    &backspace_override,
+	&copypaste_override,
+	&grave_esc_override,
+	&snip_tool_override,
+	&arc_app_1_override,
+	&arc_app_2_override,
+	&arc_app_3_override,
+    NULL
+};
+
+// (TD) Tap Dance definitions:
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_V] = ACTION_TAP_DANCE_DOUBLE(LCMD(KC_V), HYPR(KC_V)),
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+		case MACST: 
+			if (record->event.pressed) SEND_STRING(SS_LCMD(SS_LCTL(SS_LSFT("4"))));
+			break;
+		case ARCST: 
+			if (record->event.pressed) SEND_STRING(SS_LCMD(SS_LSFT("2")));
+			break;
+		case ARC1:
+			if (record->event.pressed) SEND_STRING(SS_LCMD("1"));
+			break;
+		case ARC2:
+			if (record->event.pressed) SEND_STRING(SS_LCMD("2"));
+			break;
+		case ARC3:
+			if (record->event.pressed) SEND_STRING(SS_LCMD("3"));
+			break;
+		case ARC4:
+			if (record->event.pressed) SEND_STRING(SS_LCMD("4"));
+			break;
+		case ARC5:
+			if (record->event.pressed) SEND_STRING(SS_LCMD("5"));
+			break;
+		case ARC6:
+			if (record->event.pressed) SEND_STRING(SS_LCMD("6"));
+			break;
+		case APP1:
+			if (record->event.pressed) SEND_STRING(SS_HYPER("1"));
+			break;
+		case APP2:
+			if (record->event.pressed) SEND_STRING(SS_HYPER("2"));
+			break;
+		case APP3:
+			if (record->event.pressed) SEND_STRING(SS_HYPER("3"));
+			break;
+		case AMSL:
+			if (record->event.pressed) SEND_STRING(SS_LCTL(SS_LOPT("<")));
+			break;
+		case AMSM:
+			if (record->event.pressed) SEND_STRING(SS_LCTL(SS_LOPT("\n")));
+			break;
+		case AMIN:
+			if (record->event.pressed) SEND_STRING(SS_TAP(X_LCTL(X_LOPT(X_UP))));
+			break;
+		case AMDE:
+			if (record->event.pressed) SEND_STRING(SS_TAP(X_LCTL(X_LOPT(X_DOWN))));
+			break;
+    }
+    return true;
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_BASE] = LAYOUT_split_3x6_3( \
+	[_BASE] = LAYOUT_split_3x6_3(                                                                                                                       \
   	/* ╭━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━╮                      ╭━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━╮
   	*/     KC_ESC,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                            KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,   KC_DEL, \
   	/*  ━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫                      ┣━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫
   	*/     KC_TAB,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                            KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN, KC_QUOTE, \
   	/*  ━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫                      ┣━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫
-  	*/    KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                            KC_N,     KC_M, KC_COMMA,   KC_DOT, KC_SLASH,  XXXXXXX, \
+  	*/    KC_LSFT,     KC_Z,     KC_X,     KC_C, TD(TD_V),     KC_B,                            KC_N,     KC_M, KC_COMMA,   KC_DOT, KC_SLASH,  KC_HYPR, \
   	/* ╰━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╮  ╭━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━╯
-  	*/                                            KC_LOPT,  KC_LCMD,  XXXXXXX,     XXXXXXX,  XXXXXXX,  KC_RCTL									   	    \
+  	*/                                            KC_LOPT,  KC_LCMD, L1_THUMB,    R1_THUMB, R2_THUMB,  KC_RCTL									   	    \
   	//      								   ╰─━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━╯  ╰━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━╯
  	),
-	[_NUMS] = LAYOUT_split_3x6_3( \
+	[_NUMS] = LAYOUT_split_3x6_3(                                                                                                                       \
   	/* ╭━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━╮                      ╭━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━╮
   	*/   KC_GRAVE,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                            KC_6,     KC_7,     KC_8,     KC_9,     KC_0,   KC_DEL, \
   	/*  ━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫                      ┣━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫
-  	*/    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                         XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_BSLS,  KC_MINS,   KC_EQL, \
+  	*/    _______,    MACST,     AMIN,     ARC1,     ARC2,     ARC3,                         XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_BSLS,  KC_MINS,   KC_EQL, \
   	/*  ━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫                      ┣━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫
-  	*/    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                         XXXXXXX,  XXXXXXX, KC_COMMA,   KC_DOT, KC_SLASH,  _______, \
+  	*/    _______,     AMSM,     AMDE,     ARC4,     ARC5,     ARC6,                         XXXXXXX,  XXXXXXX, KC_COMMA,   KC_DOT, KC_SLASH,  _______, \
   	/* ╰━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╮  ╭━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━╯
   	*/                                            _______,  _______,  _______,     _______,  _______,  _______									   	    \
   	//      								   ╰─━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━╯  ╰━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━╯
  	),
-	[_SYMS] = LAYOUT_split_3x6_3( \
+	[_SYMS] = LAYOUT_split_3x6_3(                                                                                                                       \
   	/* ╭━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━╮                      ╭━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━╮
   	*/    KC_TILD,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,                         KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,   KC_DEL, \
   	/*  ━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫                      ┣━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫
@@ -40,9 +160,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   	*/                                            _______,  _______,  _______,     _______,  _______,  _______									   	    \
   	//      								   ╰─━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━╯  ╰━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━╯
  	),
-	[_MOVE] = LAYOUT_split_3x6_3( \
+	[_MOVE] = LAYOUT_split_3x6_3(                                                                                                                       \
   	/* ╭━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━╮                      ╭━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━╮
-  	*/    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                         XXXXXXX,  XXXXXXX,    KC_UP,  XXXXXXX,  XXXXXXX,   KC_DEL, \
+  	*/    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                         XXXXXXX, LHS_WORD,    KC_UP, RHS_WORD,  XXXXXXX,   KC_DEL, \
   	/*  ━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫                      ┣━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫
   	*/    _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                         XXXXXXX,  KC_LEFT,  KC_DOWN, KC_RIGHT,  XXXXXXX,  _______, \
   	/*  ━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫                      ┣━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━╋━━━━━━━━━┫
