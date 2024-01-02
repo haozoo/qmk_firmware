529fbc80bf2fff1d7db4326aab46edafd4b3fb21
Hao Xu <b.hao.xu@gmail.com>
2024-01-02 13:07:51 +1100

    turned off backlighting

--- keyboards/boardsource/unicorne/keymaps/haozoo/config.h
+++ keyboards/boardsource/unicorne/keymaps/haozoo/config.h
@@ -1,2 +1,7 @@
+<<<<<<< Updated upstream
 #define ENABLE_COMPILE_KEYCODEW
 #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+=======
+#define BACKLIGHT_DEFAULT_ON false
+#define ENABLE_COMPILE_KEYCODE
+>>>>>>> Stashed changes
