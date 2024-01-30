b7cb78531110b98d1f4789cd014a58d40c7862b0
Hao Xu <hxu@atlassian.com>
2024-01-30 13:47:34 +1100

    fix

--- keyboards/boardsource/unicorne/keymaps/haozoo/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/haozoo/keymap.c
@@ -103,7 +103,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 layer_off(_FKEY);
                 if (key_pressed) break;
                 if (timer_elapsed(FHYP_timer) < TAPPING_TERM) {
-                    add_oneshot_mods(MOD_BIT(KC_HYPR));
+                    add_oneshot_mods(MOD_MASK_CSAG);
                 }
             }
             break;
